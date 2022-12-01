8478888d047e446e211efc2769ad24001bf73877
Kirill Chernogrivov <kirill.2201@inbox.ru>
2022-12-01 14:05:40 +0700

    refactor to vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,139 @@
+<!-- @format -->
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header class="header">
+    <div class="header__wrapper">
+      <h1 class="header__logo">Rhythmic</h1>
+      <div class="nav header__nav">
+        <ul class="nav__list">
+          <li class="nav__element"><a href="#" class="nav__link">ABOUT</a></li>
+          <li class="nav__element">
+            <a href="#" class="nav__link">CONTACT US</a>
+          </li>
+        </ul>
+      </div>
+      <div class="header__search">
+        <label for="search">
+          <input
+            class="header__searchField"
+            type="text"
+            name="search"
+            placeholder="SEARCH..."
+          />
+        </label>
+        <button type="submit" class="form__submit">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.7479 18.5675L14.0096 12.8251C15.1409 11.4684 15.8213 9.73459 15.8213 7.84095C15.8213 3.52083 12.2758 0.00817871 7.91475 0.00817871C3.55364 0.00817871 0 3.52493 0 7.84505C0 12.1652 3.54544 15.6778 7.90655 15.6778C9.7592 15.6778 11.4643 15.0425 12.8169 13.9809L18.5757 19.7397C18.9118 20.0758 19.4118 20.0758 19.7479 19.7397C20.084 19.4036 20.084 18.9036 19.7479 18.5675ZM1.6805 7.84505C1.6805 4.45126 4.47587 1.69278 7.90655 1.69278C11.3372 1.69278 14.1326 4.45126 14.1326 7.84505C14.1326 11.2388 11.3372 13.9973 7.90655 13.9973C4.47587 13.9973 1.6805 11.2347 1.6805 7.84505Z"
+              fill="#B3B3B3"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="header__account">
+        <a href="#">login</a>
+      </div>
+    </div>
+  </header>
   <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Righteous";
 }
 
-nav {
-  padding: 30px;
+.header {
+  width: 100%;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  background: #1e1e1e;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.header__wrapper {
+  width: 80%;
+  padding: 1rem 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav__list {
+  display: flex;
+  list-style: none;
+  gap: 30px;
+}
+
+.nav__link {
+  text-decoration: none;
+  color: #b3b3b3;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 30px;
+  color: #b3b3b3;
+}
+
+.header__logo {
+  font-family: "Righteous";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 2rem;
+  line-height: 60px;
+  color: #1db954;
+  text-transform: uppercase;
+}
+
+.header__account {
+  font-family: "Righteous";
+  font-style: normal;
+  font-size: 1rem;
+  line-height: 30px;
+  font-weight: 800;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  color: #d9d9d9;
+  border: 1px solid #b3b3b3;
+  border-radius: 10px;
+}
+
+.header__account > a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.4rem 2rem;
+  text-decoration: none;
+  color: #d9d9d9;
+  font-family: "Righteous";
+  font-size: 1rem;
+  line-height: 29.8px;
+}
+
+.header__searchField {
+  font-size: 1.2rem;
+  background-color: inherit;
+  color: #b3b3b3;
+  border: 0;
+  border-bottom: 1px solid #535353;
+  outline: 0 !important;
+}
+
+.form__submit {
+  background-color: inherit;
+  outline: 0 !important;
+  border: 0;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,328 @@
+<!-- @format -->
+
+<template>
+  <h1>{{ msg }}</h1>
+  <main class="main">
+    <div class="section">
+      <ul class="section__list">
+        <li class="section__element"><a href="#"></a> Overview</li>
+        <li class="section__element"><a href="#"></a> My music</li>
+        <li class="section__element"><a href="#"></a> Top charts</li>
+        <li class="section__element"><a href="#"></a> Newest</li>
+        <li class="section__element"><a href="#"></a> Artists</li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2 class="section__title">Artists</h2>
+      <div class="slider-container">
+        <div class="arrow-left">
+          <div class="arrow-bg">
+            <div class="arrow"></div>
+          </div>
+        </div>
+        <div class="arrow-right">
+          <div class="arrow-bg">
+            <div class="arrow"></div>
+          </div>
+        </div>
+        <div class="slider-track">
+          <div class="slider-item">
+            <div class="card section__card"></div>
+            <div class="card__title">
+              НазваниеНазваниеНазваниеНазвание какого то автора автора автора
+              автора автора
+            </div>
+          </div>
+          <div class="slider-item">
+            <div class="section__card"></div>
+          </div>
+          <div class="slider-item">
+            <div class="section__card"></div>
+          </div>
+          <div class="slider-item">
+            <div class="section__card"></div>
+          </div>
+          <div class="slider-item">
+            <div class="section__card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section__tracks">
+      <h2 class="section__title">Tracks</h2>
+      <div class="trackList">
+        <div class="trackList__track"></div>
+        <div class="trackList__track"></div>
+        <div class="trackList__track"></div>
+        <div class="trackList__track"></div>
+        <div class="trackList__track"></div>
+        <div class="trackList__track"></div>
+      </div>
+    </div>
+  </main>
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  name: "/homePage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Righteous";
+}
+
+.header {
+  width: 100%;
+}
+
+body {
+  background: #1e1e1e;
+}
+
+.header__wrapper {
+  width: 80%;
+  padding: 1rem 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav__list {
+  display: flex;
+  list-style: none;
+  gap: 30px;
+}
+
+.nav__link {
+  text-decoration: none;
+  color: #b3b3b3;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 30px;
+  color: #b3b3b3;
+}
+
+.header__logo {
+  font-family: "Righteous";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 2rem;
+  line-height: 60px;
+  color: #1db954;
+  text-transform: uppercase;
+}
+
+.header__account {
+  font-family: "Righteous";
+  font-style: normal;
+  font-size: 1rem;
+  line-height: 30px;
+  font-weight: 800;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  color: #d9d9d9;
+  border: 1px solid #b3b3b3;
+  border-radius: 10px;
+}
+
+.header__account > a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.4rem 2rem;
+  text-decoration: none;
+  color: #d9d9d9;
+  font-family: "Righteous";
+  font-size: 1rem;
+  line-height: 29.8px;
+}
+
+.header__searchField {
+  font-size: 1.2rem;
+  background-color: inherit;
+  color: #b3b3b3;
+  border: 0;
+  border-bottom: 1px solid #535353;
+  outline: 0 !important;
+}
+
+.form__submit {
+  background-color: inherit;
+  outline: 0 !important;
+  border: 0;
+  cursor: pointer;
+}
+
+/* main start */
+
+.main {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.section {
+  margin-top: 2rem;
+}
+
+.section__title {
+  font-size: 2rem;
+  color: #b3b3b3;
+  margin-bottom: 15px;
+}
+
+.form__header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.section__list {
+  font-size: 1.8rem;
+  margin-top: 5vh;
+  display: flex;
+  list-style: none;
+  width: 100%;
+  border-bottom: 1px solid #535353;
+}
+
+.section__element {
+  color: #535353;
+  position: relative;
+  top: 1px;
+  display: flex;
+  padding: 0 3rem 0 3rem;
+  text-decoration: none;
+  border-bottom: 1px solid #535353;
+  transition: 0.5s;
+}
+
+.section__element:hover {
+  border-color: #1db954;
+  color: #b3b3b3;
+}
+
+.section__cards {
+  display: block;
+  width: 100%;
+}
+
+.section__card {
+  display: inline-block;
+  position: relative;
+  border-radius: 10px;
+  background-color: #fff;
+  height: 15rem;
+  width: 14rem;
+}
+
+.card__title {
+  color: #b3b3b3;
+  font-size: 1rem;
+  width: 14rem;
+  height: 1.2rem;
+  overflow-wrap: break-word;
+}
+
+.slider-item {
+  margin-right: 5vw;
+}
+
+.slider-container {
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.slider-track {
+  display: flex;
+  transition-duration: 0.5s;
+}
+
+.arrow {
+  width: 7px;
+  height: 7px;
+  border-top: #cacaca 3px solid;
+  border-right: #cacaca 3px solid;
+  position: absolute;
+}
+.arrow-bg {
+  width: 30px;
+  height: 30px;
+  background: #fff;
+  box-shadow: 0 0 4px 2px rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  position: relative;
+  opacity: 0.8;
+  cursor: pointer;
+}
+.arrow-left .arrow {
+  transform: rotate(-135deg);
+  top: 11px;
+  left: 11px;
+}
+.arrow-right .arrow {
+  transform: rotate(45deg);
+  top: 11px;
+  left: 10px;
+}
+.arrow-left {
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  z-index: 2;
+}
+.arrow-right {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  z-index: 2;
+}
+
+.trackList {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(390px, 1fr));
+  justify-content: space-between;
+  gap: 30px;
+}
+
+.trackList__track {
+  height: 66px;
+  border: 1px solid #b3b3b3;
+  border-radius: 10px;
+  background-color: #212121;
+}
+
+@media screen and (max-width: 1140px) {
+  * {
+    font-size: 12px;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  .trackList {
+    grid-template-columns: repeat(auto-fill, 100%);
+  }
+}
+
+@media screen and (max-width: 770px) {
+  .header__nav,
+  .header__search {
+    display: none;
+  }
+}
+</style>
